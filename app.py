5e987c23981960f4165b2b34e725e0e8e5c848a3
Valentin Ionescu <valiyuneski@gmail.com>
2025-09-18 11:02:05 +0200

    Designing the Routes and Data Structure

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    with open("blog_posts.json", "r") as f:
+        blog_posts = json.load(f)
+    return render_template("index.html", posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=9090, debug=True)

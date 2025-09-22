47e2feea50636e9c979e7905af0fd223f31dcd08
Valentin Ionescu <valiyuneski@gmail.com>
2025-09-22 11:20:25 +0200

    Fixed the Erros regarding delete_html. index now has all buttons inside and is hopefully correct

--- app.py
+++ app.py
@@ -69,24 +69,41 @@ def add():
     return render_template('add.html')
 
 
-@app.route("/delete/<int:post_id>", methods=["GET", "POST"])
+# @app.route("/delete/<int:post_id>", methods=["GET", "POST"])
+# def delete(post_id):
+#     """Delete a blog post by ID."""
+#     blog_posts = load_blog_posts()
+
+#     # Find the post to delete
+#     post = next((p for p in blog_posts if p["id"] == post_id), None)
+
+#     # Second Navigation after actual delete will be back to index PAGE
+#     if request.method == "POST":
+#         if post:
+#             # Remove the post from the list
+#             blog_posts = [p for p in blog_posts if p.get("id") != post_id]
+#             save_blog_posts(blog_posts)
+#         return redirect(url_for("index"))
+
+#     # First Navigation is: GET request (just for confirmation of deletetion)
+#     return render_template("delete.html", post=post)
+@app.route("/delete/<int:post_id>")
 def delete(post_id):
     """Delete a blog post by ID."""
+
+    # Get all posts
     blog_posts = load_blog_posts()
 
-    # Find the post to delete
-    post = next((p for p in blog_posts if p["id"] == post_id), None)
+    # Find the post to delete by ID
+    post_to_delete = next((p for p in blog_posts if p["id"] == post_id), None)
 
-    # Second Navigation after actual delete will be back to index PAGE
-    if request.method == "POST":
-        if post:
-            # Remove the post from the list
-            blog_posts = [p for p in blog_posts if p.get("id") != post_id]
-            save_blog_posts(blog_posts)
-        return redirect(url_for("index"))
+    if post_to_delete:
+        # Remove the post from the list
+        blog_posts = [p for p in blog_posts if p.get("id") != post_id]
 
-    # First Navigation is: GET request (just for confirmation of deletetion)
-    return render_template("delete.html", post=post)
+        # Save back to JSON
+        save_blog_posts(blog_posts)
+    return redirect(url_for("index"))
 
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
